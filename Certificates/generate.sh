8f5ec3fa6eeee3981b1a146e7888cef61166e2cc
Maxim Shapovalov <mrypdm@gmail.com>
2025-10-13 12:10:30 +0300

    fix certs for kafka

--- Certificates/generate.sh
+++ Certificates/generate.sh
@@ -40,6 +40,19 @@ function generate_root_certificate() {
     rm root/root.csr
 }
 
+function create_java_truststore() {
+    echo "Enter password for truststore: "
+    read -s truststore_password
+
+    umask u=rw,go= && \
+        keytool \
+            -keystore root/root.jks \
+            -alias root \
+            -storepass "$truststore_password" \
+            -importcert -file root/root.crt \
+            -noprompt
+}
+
 function generate_server_certificate() {
     server_name=$1
 
@@ -103,10 +116,77 @@ function generate_client_certificate() {
             -out $server_name/$client_name/$client_name.crt
 
     rm $server_name/$client_name/$client_name.csr
-
 }
 
+function append_to_server_keystore(){
+    keystore=$1
+    cn=$2
+    pass=$3
+
+    umask u=rw,go= && keytool -keystore $keystore -alias $cn -validity 365 -genkey -keyalg RSA -dname "$MTLS_DN_S""$cn" -storepass $pass -keypass $pass
+    umask u=rw,go= && keytool -keystore $keystore -alias $cn -certreq -file $cn.csr -storepass $pass -keypass $pass
+    umask u=rw,go= && openssl x509 \
+        -req \
+        -text \
+        -CAcreateserial \
+        -days $DURATION_DAYS \
+        -in $cn.csr \
+        -extfile <(cat /etc/ssl/openssl.cnf openssl.cnf) \
+        -extensions v3_mtls_server_kafka \
+        -CA root/root.crt \
+        -CAkey root/root.key \
+        -out $cn.crt
+    umask u=rw,go= && keytool -keystore $keystore -alias $cn -import -file $cn.crt -storepass $pass -keypass $pass -noprompt
+    rm $cn.csr $cn.crt
+}
+
+function append_to_user_keystore(){
+    keystore=$1
+    cn=$2
+
+    umask u=rw,go= && keytool -keystore $keystore -alias $cn -validity 365 -genkey -keyalg RSA -dname "$MTLS_DN_S""$cn" -storepass $pass -keypass $pass
+    umask u=rw,go= && keytool -keystore $keystore -alias $cn -certreq -file $cn.csr -storepass $pass -keypass $pass
+    umask u=rw,go= && openssl x509 \
+        -req \
+        -text \
+        -CAcreateserial \
+        -days $DURATION_DAYS \
+        -in $cn.csr \
+        -extfile <(cat /etc/ssl/openssl.cnf openssl.cnf) \
+        -extensions v3_mtls_client \
+        -CA root/root.crt \
+        -CAkey root/root.key \
+        -out $cn.crt
+    umask u=rw,go= && keytool -keystore $keystore -alias $cn -import -file $cn.crt -storepass $pass -keypass $pass -noprompt
+    rm $cn.csr $cn.crt
+}
+
+function generate_kafka_server_certificate() {
+    umask u=rwx,go= && mkdir -p kafka
+
+    echo "Enter password for Kafka keystore: "
+    read -s keystore_password
+
+    umask u=rw,go= && keytool -keystore kafka/kafka.keystore.jks -alias caroot -storepass $keystore_password -importcert -file root/root.crt -noprompt
+    append_to_server_keystore kafka/kafka.keystore.jks kafka $keystore_password
+    append_to_server_keystore kafka/kafka.keystore.jks localhost $keystore_password
+}
+
+function generate_kafka_client_certificate() {
+    client_name=$1
+
+    umask u=rwx,go= && mkdir -p kafka/$client_name
+
+    echo "Enter password for Kafka $client_name keystore: "
+    read -s keystore_password
+
+    umask u=rw,go= && keytool -keystore kafka/$client_name/$client_name.keystore.jks -alias caroot -storepass $keystore_password -importcert -file root/root.crt -noprompt
+    append_to_user_keystore kafka/$client_name/$client_name.keystore.jks $client_name $keystore_password
+}
+
+
 generate_root_certificate
+create_java_truststore
 
 generate_server_certificate postgres
 generate_client_certificate postgres superuser
@@ -114,9 +194,7 @@ generate_client_certificate postgres svc_jobs_webapi
 generate_client_certificate postgres svc_jobs_worker
 generate_client_certificate postgres svc_users_webapp
 
-
-generate_server_certificate zookeeper
-generate_server_certificate kafka
-generate_client_certificate kafka superuser
-generate_client_certificate kafka svc_jobs_webapi
-generate_client_certificate kafka svc_jobs_worker
+generate_kafka_server_certificate
+generate_kafka_client_certificate superuser
+generate_kafka_client_certificate svc_jobs_webapi
+generate_kafka_client_certificate svc_jobs_worker
