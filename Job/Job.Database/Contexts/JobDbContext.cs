07f19439c80cf20c03a107bbff381682f6b33a37
Maxim Shapovalov <mrypdm@gmail.com>
2025-10-16 12:03:14 +0300

    database methods

--- Job/Job.Database/Contexts/JobDbContext.cs
+++ Job/Job.Database/Contexts/JobDbContext.cs
@@ -1,3 +1,7 @@
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Job.Contract;
 using Job.Database.Models;
 using Microsoft.EntityFrameworkCore;
 using Npgsql;
@@ -14,6 +18,53 @@ public class JobDbContext(DbContextOptions options) : DbContext(options)
     /// Table of <see cref="JobDbModel"/>
     /// </summary>
     public DbSet<JobDbModel> Jobs { get; set; }
+
+    /// <summary>
+    /// Add new Job to database
+    /// </summary>
+    public async Task AddNewJobAsync(CreateJobRequest job, CancellationToken cancellationToken)
+    {
+        await Database
+            .ExecuteSqlAsync($"exec p_jobs_addnew {job.Id} {job.Timeout} {job.Steps}", cancellationToken);
+    }
+
+    /// <summary>
+    /// Get Job for running
+    /// </summary>
+    public async Task<CreateJobRequest> GetNewJobAsync(Guid jobId, CancellationToken cancellationToken)
+    {
+        return await Database
+            .SqlQuery<CreateJobRequest>($"exec p_jobs_getnew {jobId}")
+            .SingleOrDefaultAsync(cancellationToken);
+    }
+
+    /// <summary>
+    /// Set Job as running
+    /// </summary>
+    public async Task SetJobRunningAsync(Guid jobId, CancellationToken cancellationToken)
+    {
+        await Database
+            .ExecuteSqlAsync($"exec p_jobs_setrunning {jobId}", cancellationToken);
+    }
+
+    /// <summary>
+    /// Set Job results
+    /// </summary>
+    public async Task SetJobResultsAsync(Guid jobId, JobStatus jobStatus, byte[] results, CancellationToken cancellationToken)
+    {
+        await Database
+            .ExecuteSqlAsync($"exec p_jobs_setresults {jobId} {jobStatus} {results}", cancellationToken);
+    }
+
+    /// <summary>
+    /// Get Job results
+    /// </summary>
+    public async Task<JobResultResponse> GetJobResults(Guid jobId, CancellationToken cancellationToken)
+    {
+        return await Database
+            .SqlQuery<JobResultResponse>($"exec p_jobs_getresults {jobId}")
+            .SingleOrDefaultAsync(cancellationToken);
+    }
 
     /// <inheritdoc />
     protected override void OnModelCreating(ModelBuilder modelBuilder)
