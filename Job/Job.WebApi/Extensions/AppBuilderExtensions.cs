cdbf37c36e95074ad7987aaf95c8ed25b755cc13
Maxim Shapovalov <mrypdm@gmail.com>
2025-10-18 16:50:24 +0300

    extensions

---
+++ Job/Job.WebApi/Extensions/AppBuilderExtensions.cs
@@ -0,0 +1,127 @@
+using System.Reflection;
+using System.Security.Claims;
+using System.Security.Cryptography.X509Certificates;
+using Job.Broker;
+using Job.Broker.Options;
+using Job.Database.Contexts;
+using Microsoft.AspNetCore.Authentication.Certificate;
+using Microsoft.AspNetCore.Server.Kestrel.Core;
+using Microsoft.AspNetCore.Server.Kestrel.Https;
+using Shared.Contract;
+using Shared.Contract.Options;
+using Shared.Database;
+
+namespace Job.WebApi.Extensions;
+
+/// <summary>
+/// Extensions for <see cref="WebApplicationBuilder"/>
+/// </summary>
+public static class AppBuilderExtensions
+{
+    /// <summary>
+    /// Add Swagger
+    /// </summary>
+    public static WebApplicationBuilder AddSwagger(this WebApplicationBuilder builder)
+    {
+        builder.Services
+            .AddEndpointsApiExplorer()
+            .AddSwaggerGen(opt =>
+            {
+                opt.IncludeXmlComments(
+                    Path.Combine(AppContext.BaseDirectory, $"{Assembly.GetExecutingAssembly().GetName().Name}.xml"));
+            });
+        return builder;
+    }
+
+    /// <summary>
+    /// Add Database
+    /// </summary>
+    public static WebApplicationBuilder AddDatabase(this WebApplicationBuilder builder)
+    {
+        var dbOptions = builder.Configuration.GetSection("DatabaseOptions").Get<DatabaseOptions>();
+        builder.Services.AddDbContext<JobDbContext>(options => JobDbContext.BuildOptions(options, dbOptions));
+        return builder;
+    }
+
+    /// <summary>
+    /// Add Broker
+    /// </summary>
+    public static WebApplicationBuilder AddBroker(this WebApplicationBuilder builder)
+    {
+        var producerOptions = builder.Configuration.GetSection("ProducerOptions").Get<ProducerOptions>();
+        builder.Services.AddSingleton(producerOptions);
+        builder.Services.AddSingleton<JobProducer>();
+        return builder;
+    }
+
+    /// <summary>
+    /// Configure HTTPS options
+    /// </summary>
+    public static WebApplicationBuilder ConfigureHttps(this WebApplicationBuilder builder)
+    {
+        var webServerOptions = builder.Configuration.GetSection("WebServerOptions").Get<WebServerOptions>();
+        builder.Services.Configure<KestrelServerOptions>(kestrelOptions =>
+        {
+            kestrelOptions.ConfigureHttpsDefaults(httpsOptions =>
+            {
+                httpsOptions.ClientCertificateMode = ClientCertificateMode.RequireCertificate;
+                httpsOptions.ServerCertificate = webServerOptions.Certificate;
+                httpsOptions.ServerCertificateChain = webServerOptions.CertificateChain;
+            });
+        });
+        return builder;
+    }
+
+    /// <summary>
+    /// Add certificate authentication
+    /// </summary>
+    public static WebApplicationBuilder AddCertificateAuthentication(this WebApplicationBuilder builder)
+    {
+        var webServerOptions = builder.Configuration.GetSection("WebServerOptions").Get<WebServerOptions>();
+        var sslValidator = new SslValidator(webServerOptions);
+        builder.Services
+            .AddAuthentication(CertificateAuthenticationDefaults.AuthenticationScheme)
+            .AddCertificate(certOptions =>
+            {
+                certOptions.AllowedCertificateTypes = CertificateTypes.Chained;
+                certOptions.ChainTrustValidationMode = X509ChainTrustMode.CustomRootTrust;
+                certOptions.CustomTrustStore = webServerOptions.CertificateChain;
+                certOptions.RevocationMode = X509RevocationMode.NoCheck;
+                certOptions.ValidateValidityPeriod = true;
+                certOptions.ValidateCertificateUse = true;
+                certOptions.Events = new CertificateAuthenticationEvents
+                {
+                    OnCertificateValidated = context =>
+                    {
+                        if (sslValidator.IsRevoked(context.ClientCertificate))
+                        {
+                            context.Fail("Client certificated is revoked");
+                            return Task.CompletedTask;
+                        }
+
+                        context.Principal = new ClaimsPrincipal(new ClaimsIdentity(
+                        [
+                            new(ClaimTypes.Name,
+                                context.ClientCertificate.GetNameInfo(X509NameType.SimpleName, forIssuer: false)),
+                            new(ClaimTypes.X500DistinguishedName, context.ClientCertificate.Subject)
+                        ], context.Scheme.Name));
+
+                        context.Success();
+                        return Task.CompletedTask;
+                    },
+                    OnAuthenticationFailed = context =>
+                    {
+                        context.Fail(context.Exception.Message);
+                        return Task.CompletedTask;
+                    }
+                };
+            })
+            .AddCertificateCache(options =>
+            {
+                options.CacheEntryExpiration = TimeSpan.FromMinutes(15);
+                options.CacheSize = 100;
+            });
+        builder.Services.AddAuthorization(opt => opt.FallbackPolicy = opt.DefaultPolicy);
+        return builder;
+    }
+}
--- Job/Job.WebApi/Program.cs
+++ Job/Job.WebApi/Program.cs
@@ -1,98 +1,13 @@
-using System.Reflection;
-using System.Security.Claims;
-using System.Security.Cryptography.X509Certificates;
-using Job.Broker;
-using Job.Broker.Options;
-using Job.Database.Contexts;
-using Microsoft.AspNetCore.Authentication.Certificate;
-using Microsoft.AspNetCore.Server.Kestrel.Core;
-using Microsoft.AspNetCore.Server.Kestrel.Https;
-using Shared.Contract;
-using Shared.Contract.Options;
-using Shared.Database;
+using Job.WebApi.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
-builder.Services
-    .AddEndpointsApiExplorer()
-    .AddSwaggerGen(opt =>
-    {
-        opt.IncludeXmlComments(
-            Path.Combine(AppContext.BaseDirectory, $"{Assembly.GetExecutingAssembly().GetName().Name}.xml"));
-    });
 
-var webServerOptions = builder.Configuration.GetSection("WebServerOptions").Get<WebServerOptions>();
-var sslValidator = new SslValidator(webServerOptions);
-builder.Services.Configure<KestrelServerOptions>(options =>
-{
-    options.ConfigureHttpsDefaults(httpsOptions =>
-    {
-        httpsOptions.ClientCertificateMode = ClientCertificateMode.RequireCertificate;
-        httpsOptions.ServerCertificate = webServerOptions.Certificate;
-        httpsOptions.ServerCertificateChain = webServerOptions.CertificateChain;
-    });
-});
-
-builder.Services
-    .AddAuthentication(CertificateAuthenticationDefaults.AuthenticationScheme)
-    .AddCertificate(options =>
-    {
-        options.AllowedCertificateTypes = CertificateTypes.Chained;
-        options.ChainTrustValidationMode = X509ChainTrustMode.CustomRootTrust;
-        options.CustomTrustStore = webServerOptions.CertificateChain;
-        options.RevocationMode = X509RevocationMode.NoCheck;
-        options.ValidateValidityPeriod = true;
-        options.ValidateCertificateUse = true;
-
-        options.Events = new CertificateAuthenticationEvents
-        {
-            OnCertificateValidated = context =>
-            {
-                if (sslValidator.IsRevoked(context.ClientCertificate))
-                {
-                    context.Fail("Client certificated is revoked");
-                    return Task.CompletedTask;
-                }
-
-                var claims = new Claim[]
-                {
-                    new(ClaimTypes.Name,
-                        context.ClientCertificate.GetNameInfo(X509NameType.SimpleName, forIssuer: false),
-                        ClaimValueTypes.String,
-                        context.ClientCertificate.Issuer),
-                    new(ClaimTypes.X500DistinguishedName,
-                        context.ClientCertificate.Subject,
-                        ClaimValueTypes.String,
-                        context.ClientCertificate.Issuer)
-                };
-
-                context.Principal = new ClaimsPrincipal(new ClaimsIdentity(claims, context.Scheme.Name));
-
-                context.Success();
-                return Task.CompletedTask;
-            },
-
-            OnAuthenticationFailed = context =>
-            {
-                context.Fail(context.Exception.Message);
-                return Task.CompletedTask;
-            }
-        };
-    })
-    .AddCertificateCache(options =>
-    {
-        options.CacheEntryExpiration = TimeSpan.FromMinutes(15);
-        options.CacheSize = 100;
-    });
-builder.Services.AddAuthorization(opt => opt.FallbackPolicy = opt.DefaultPolicy);
-
+builder.AddSwagger()
+    .ConfigureHttps()
+    .AddCertificateAuthentication()
+    .AddDatabase()
+    .AddBroker();
 builder.Services.AddControllers();
-
-var dbOptions = builder.Configuration.GetSection("DatabaseOptions").Get<DatabaseOptions>();
-builder.Services.AddDbContext<JobDbContext>(options => JobDbContext.BuildOptions(options, dbOptions));
-
-var producerOptions = builder.Configuration.GetSection("ProducerOptions").Get<ProducerOptions>();
-builder.Services.AddSingleton(producerOptions);
-builder.Services.AddSingleton<JobProducer>();
 
 var application = builder.Build();
 
@@ -113,5 +28,4 @@ if (application.Environment.IsDevelopment())
 }
 
 application.MapControllers();
-
 application.Run();
