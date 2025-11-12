9a16ba9cadb483e435cd1f570f586968123cfc96
Maxim Shapovalov <mrypdm@gmail.com>
2025-11-12 09:55:22 +0300

    movements

--- src/Job/Job.Worker/Properties/AssemblyInfo.cs
+++ src/Job/Job.Worker/Properties/AssemblyInfo.cs
@@ -4,4 +4,4 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 [assembly: Guid("a609a410-724a-4c1c-85ec-f173ef205b1e")]
 
-[assembly: InternalsVisibleTo("Job.Worker.Tests")]
+[assembly: InternalsVisibleTo("Job.Worker.UnitTests")]
