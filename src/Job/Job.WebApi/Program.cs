0aea80d82b471a6739e40c950dd7db4d59e4d5b1
Maxim Shapovalov <mrypdm@gmail.com>
2025-11-16 19:48:33 +0300

    debug env vars

--- src/Job/Job.WebApi/Program.cs
+++ src/Job/Job.WebApi/Program.cs
@@ -1,4 +1,33 @@
+using System.Net;
 using Job.WebApi.Extensions;
+
+var envVars = Environment.GetEnvironmentVariables();
+foreach (var env in envVars.Keys)
+{
+    Console.WriteLine($"{env} = {envVars[env]}");
+}
+
+if (envVars.Contains("ProducerOptions:Servers"))
+{
+    var server = (string)envVars["ProducerOptions:Servers"];
+
+    if (server != "kafka:8500")
+    {
+        throw new Exception($"invalid server: {server}");
+    }
+
+    var serverIps = Dns.GetHostAddresses("kafka");
+    foreach (var ip in serverIps)
+    {
+        Console.WriteLine(ip);
+    }
+}
+else
+{
+    throw new Exception("there is no kafka servers");
+}
+
+return;
 
 var builder = WebApplication.CreateBuilder(args);
 
