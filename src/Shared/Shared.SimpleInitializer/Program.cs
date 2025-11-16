531f309063769c29c8b0151adc29c9e8366f7431
Maxim Shapovalov <mrypdm@gmail.com>
2025-11-16 13:52:44 +0300

    common initializer for broker and databases

---
+++ src/Shared/Shared.SimpleInitializer/Program.cs
@@ -0,0 +1,64 @@
+using Job.Broker.Clients;
+using Job.Database.Contexts;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Shared.Broker.Abstractions;
+using Shared.Broker.Options;
+using Shared.Contract;
+using Shared.Contract.Extensions;
+using Shared.Database;
+using Shared.Database.Migrations;
+using User.Database.Contexts;
+
+var builder = Host.CreateApplicationBuilder(args);
+
+builder.Services.AddLogging(builder =>
+{
+    builder.AddConsole();
+});
+
+builder.Services.AddSingleton(builder.Configuration.GetOptions<AdminOptions>());
+builder.Services.AddTransient<IBrokerAdminClient, BrokerAdminClient>();
+builder.Services.AddTransient<IInitializer>(
+    context => new BrokerInitializer(context.GetRequiredService<IBrokerAdminClient>()));
+
+var jobDbOptions = builder.Configuration.GetOptions<DatabaseOptions>("AdminJobsDatabaseOptions");
+var jobDbSslValidator = new SslValidator(jobDbOptions);
+builder.Services.AddTransient(context =>
+{
+    var options = PostgreDbContext.BuildOptions(
+        new DbContextOptionsBuilder(),
+        jobDbOptions,
+        jobDbSslValidator,
+        context.GetRequiredService<ILoggerFactory>());
+    return new JobDbContext(options.Options, context.GetRequiredService<ILogger<JobDbContext>>());
+});
+builder.Services.AddTransient<IInitializer>(
+    context => new DbInitializer(context.GetRequiredService<JobDbContext>()));
+
+var userDbOptions = builder.Configuration.GetOptions<DatabaseOptions>("AdminUsersDatabaseOptions");
+var userDbSslValidator = new SslValidator(userDbOptions);
+builder.Services.AddTransient(context =>
+{
+    var options = PostgreDbContext.BuildOptions(
+        new DbContextOptionsBuilder(),
+        userDbOptions,
+        userDbSslValidator,
+        context.GetRequiredService<ILoggerFactory>());
+    return new UserDbContext(options.Options, context.GetRequiredService<ILogger<UserDbContext>>());
+});
+builder.Services.AddTransient<IInitializer>(
+    context => new DbInitializer(context.GetRequiredService<UserDbContext>()));
+
+var host = builder.Build();
+
+var lifeTime = host.Services.GetService<IHostApplicationLifetime>()
+    ?? throw new InvalidOperationException("Cannot get application lifetime");
+foreach (var initializer in host.Services.GetServices<IInitializer>())
+{
+    await initializer.InitializeAsync(lifeTime.ApplicationStopping);
+}
+
+host.Dispose();
